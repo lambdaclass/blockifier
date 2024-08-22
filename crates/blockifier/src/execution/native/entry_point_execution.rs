ed2a4d89260d23f79b4cfa0b9ebad5c4463823a6
Edgar <git@edgarluque.com>
2024-08-22 15:47:45 +0200

    upd rust, add sierra-emu as a feature

--- crates/blockifier/src/execution/native/entry_point_execution.rs
+++ crates/blockifier/src/execution/native/entry_point_execution.rs
@@ -8,7 +8,7 @@ use crate::execution::contract_class::NativeContractClassV1;
 use crate::execution::entry_point::{
     CallEntryPoint, EntryPointExecutionContext, EntryPointExecutionResult,
 };
-use crate::execution::native::utils::run_sierra_emu_executor;
+use crate::execution::native::utils::{run_native_executor, run_sierra_emu_executor};
 use crate::state::state_api::State;
 
 pub fn execute_entry_point_call(
@@ -30,15 +30,14 @@ pub fn execute_entry_point_call(
         context,
     );
 
-    println!("Blockifier-Sierra-Emu: running the Native Sierra emulator");
-    let vm = sierra_emu::VirtualMachine::new_starknet(
-        Arc::new(contract_class.program.clone()),
-        &mut syscall_handler,
-    );
-    let result2 = run_sierra_emu_executor(vm, function_id, call.clone())?;
-    // dbg!(&result2);
-    // let result = run_native_executor(&contract_class.executor, function_id, call,
-    // syscall_handler);
-    println!("Blockifier-Sierra-Emu: Sierra Emu Executor finished running");
-    Ok(result2)
+    let result = if cfg!(feature = "use-sierra-emu") {
+        let vm = sierra_emu::VirtualMachine::new_starknet(
+            Arc::new(contract_class.program.clone()),
+            &mut syscall_handler,
+        );
+        run_sierra_emu_executor(vm, function_id, call.clone())?
+    } else {
+        run_native_executor(&contract_class.executor, function_id, call, syscall_handler)?
+    };
+    Ok(result)
 }
